953795ea4e76bfcc9f6989cfd7f14b896dffa1ae
shankusu <shankusu2017@gmail.com>
2023-03-16 18:18:22 +0800

    补充版本相关的文件，以便能在windows上用 vs 编译

---
+++ config-msvc-version.h
@@ -0,0 +1,14 @@
+#define PACKAGE_NAME "OpenVPN"
+#define PACKAGE_STRING "OpenVPN 2.6.1"
+#define PACKAGE_TARNAME "openvpn"
+#define PACKAGE "openvpn"
+#define PRODUCT_VERSION_MAJOR "2"
+#define PRODUCT_VERSION_MINOR "6"
+#define PRODUCT_VERSION_PATCH ".1"
+#define PACKAGE_VERSION "2.6.1"
+#define PRODUCT_VERSION "2.6.1"
+#define PRODUCT_BUGREPORT "openvpn-users@lists.sourceforge.net"
+#define OPENVPN_VERSION_RESOURCE 2,6,1,0
+#define TAP_WIN_COMPONENT_ID "tap0901"
+#define TAP_WIN_MIN_MAJOR 9
+#define TAP_WIN_MIN_MINOR 9
---
+++ note.md
@@ -0,0 +1,10 @@
+注意事项
+
+* 抓住主干，不要在不重要的细节上浪费太多时间
+* 先看 windows 版本的代码，再看 unix-like
+* 先看 tun 设备的代码，再看 tap 的代码
+* 先看 client 逻辑的代码，再看 server 的
+
+### 编译参考手册
+
+* https://community.openvpn.net/openvpn/wiki/BuildingOnWindows
